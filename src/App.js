3b282bed04eb79ed4c7f4d7b28516934d61270da
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-03-06 17:33:41 -0600

    made card component for common css elements

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ function App() {
     {
       id: 'e1',
       title: 'Car Insurance',
-      amount: 294.67,
+      amount:294.67,
       date: new Date(2021, 2, 28)
     },
     {
       id: 'e2',
       title: 'House Payment',
-      amount: 946.32,
+      amount:978.33,
       date: new Date(2021, 2, 1)
     },
     {
       id: 'e3',
       title: 'New TV',
-      amount: 788.98,
+      amount:788.99,
       date: new Date(2021, 4, 12)
     },
     {
       id: 'e4',
       title: 'Dog Food',
-      amount: 67.99,
+      amount:67.99,
       date: new Date(2021, 4, 12)
     },
   ];
