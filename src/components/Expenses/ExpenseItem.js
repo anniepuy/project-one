37246b57968c9d3bacd5be6d170e6c8b1315359d
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-03-07 09:19:14 -0600

    Created user input form for newexpense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,14 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+    const clickHandler = () => {
+        setTitle('Updated');
+        console.log(title);
+    };
     
     return (
         <Card className="expense-item">
@@ -11,12 +17,13 @@ const ExpenseItem = (props) => {
             </div>
             <div className="expense-item__description">
                 <h2>
-                    {props.title}
+                    {title}
                 </h2>
             </div>
             <div className="expense-item__price">
                 ${props.amount}
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
